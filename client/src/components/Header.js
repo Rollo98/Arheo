b21ccd4c2910fb3f3ed04b708c461d4cbafb7677
Rolleh <32469870+Rollo98@users.noreply.github.com>
2019-06-16 16:40:13 +0300

    update header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+import { Navbar, Nav } from "react-bootstrap";
 
 import * as actions from "../actions";
 
@@ -14,90 +15,96 @@ class Header extends Component {
   }
   render() {
     return (
-      <nav className="sticky-top navbar navbar-expand-lg navbar-dark bg-dark">
-        <Link className="navbar-brand" to="/">
-          Arheo
-        </Link>
-        <div className="collapse navbar-collapse">
-          <ul className="navbar-nav ml-auto">
-            {!this.props.isAuth ? (
-              <>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/SignUp">
-                    Sign Up
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/SignIn">
-                    Sign In
-                  </Link>
-                </li>
-              </>
-            ) : null}
-            {this.props.role.includes("admin") ? (
-              <>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/">
-                    Acasa
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/Dashboard">
-                    Dashboard
-                  </Link>
-                </li>
-              </>
-            ) : null}
-            {this.props.role.includes("writer") ? (
-              <>
-                {!this.props.saveState ? (
+      <div>
+        <Navbar
+          className="sticky-top navbar text-center navbar-expand-lg navbar-dark bg-dark"
+          expand="sm"
+        >
+          <Link className="navbar-brand text-white" to="/">
+            Arheo
+          </Link>
+          <Navbar.Toggle aria-controls="navbar" />
+          <Navbar.Collapse id="navbar">
+            <Nav className="ml-auto">
+              {!this.props.isAuth ? (
+                <>
                   <li className="nav-item">
-                    <Link className="nav-link" to="/NewArcheologist">
-                      New Archeologist
+                    <Link className="nav-link" to="/SignUp">
+                      Sign Up
                     </Link>
                   </li>
-                ) : null}
-              </>
-            ) : null}
-            {this.props.isAuth ? (
-              <>
-                <li className="nav-item">
-                  <div className="dropdown">
-                    <button
-                      className="btn btn-secondary customButton dropdown-toggle"
-                      type="button"
-                      id="dropdownMenuButton"
-                      data-toggle="dropdown"
-                      aria-haspopup="true"
-                      aria-expanded="false"
-                    >
-                      {this.props.userName}
-                    </button>
-                    <div
-                      className="dropdown-menu dropdownCustom dropdown-menu-right text-center"
-                      aria-labelledby="dropdownMenuButton"
-                    >
-                      <Link
-                        className="dropdown-item dropitemCustom nav-link"
-                        to="/account"
-                      >
-                        Profil
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/SignIn">
+                      Sign In
+                    </Link>
+                  </li>
+                </>
+              ) : null}
+              {this.props.role.includes("admin") ? (
+                <>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/">
+                      Acasa
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to="/Dashboard">
+                      Dashboard
+                    </Link>
+                  </li>
+                </>
+              ) : null}
+              {this.props.role.includes("writer") ? (
+                <>
+                  {!this.props.saveState ? (
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/NewArcheologist">
+                        New Archeologist
                       </Link>
-                      <Link
-                        className="dropdown-item dropitemCustom nav-link"
-                        to="/"
-                        onClick={this.signOut}
+                    </li>
+                  ) : null}
+                </>
+              ) : null}
+              {this.props.isAuth ? (
+                <>
+                  <li className="nav-item">
+                    <div className="dropdown">
+                      <button
+                        className="btn btn-secondary customButton dropdown-toggle"
+                        type="button"
+                        id="dropdownMenuButton"
+                        data-toggle="dropdown"
+                        aria-haspopup="true"
+                        aria-expanded="false"
                       >
-                        Sign Out
-                      </Link>
+                        {this.props.userName}
+                      </button>
+                      <div
+                        className="dropdown-menu dropdownCustom dropdown-menu-right text-center"
+                        aria-labelledby="dropdownMenuButton"
+                      >
+                        <Link
+                          className="dropdown-item dropitemCustom nav-link"
+                          to="/account"
+                        >
+                          Profil
+                        </Link>
+                        <Link
+                          className="dropdown-item dropitemCustom nav-link"
+                          to="/"
+                          onClick={this.signOut}
+                        >
+                          Sign Out
+                        </Link>
+                      </div>
                     </div>
-                  </div>
-                </li>
-              </>
-            ) : null}
-          </ul>
-        </div>
-      </nav>
+                  </li>
+                </>
+              ) : null}
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
     );
   }
 }
