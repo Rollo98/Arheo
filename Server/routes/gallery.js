a243de7268b7f16980ce7cec85c3c4b4a7de5378
Eduard Tamsa <eduard.t@skyx.com>
2019-07-07 20:42:09 +0300

    fix add

--- Server/routes/gallery.js
+++ Server/routes/gallery.js
@@ -4,10 +4,15 @@ const passport = require("passport");
 
 const passportConf = require("../passport");
 const GalleryController = require("../controllers/gallery");
-
+const multer = require("multer");
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: "./uploads"
+  })
+});
 const passportJWT = passport.authenticate("jwt", { session: false });
 
-router.route("/add").post(passportJWT, GalleryController.addGallery);
+router.route("/add").post(passportJWT, upload.any(), GalleryController.addGallery);
 router
   .route("/get/:uid?")
   .get(GalleryController.getGallery)
